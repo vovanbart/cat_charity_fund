e63662dd4e3006a0a015576e55423e26efd69d55
vovanbart <vovanbart@icloud.com>
2022-11-30 22:49:16 +0300

    1

--- app/core/base.py
+++ app/core/base.py
@@ -1,2 +1,2 @@
-from app.core.db import Base, PreBaseDonationCharity
-from app.models import CharityProject, Donation, User
+from app.core.db import Base, PreBaseDonationCharity #noqa
+from app.models import CharityProject, Donation, User #noqa
