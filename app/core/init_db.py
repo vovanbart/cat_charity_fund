4b0f1419506f0c133b5a2a8fdb05c226a850e3e5
vovanbart <vovanbart@icloud.com>
2022-12-03 23:13:59 +0300

    rev

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -17,16 +17,16 @@ async def create_user(
         email: EmailStr, password: str, is_superuser: bool = False
 ):
     try:
-        async with get_async_session_context() as session:
-            async with get_user_db_context(session) as user_db:
-                async with get_user_manager_context(user_db) as user_manager:
-                    await user_manager.create(
-                        UserCreate(
-                            email=email,
-                            password=password,
-                            is_superuser=is_superuser
-                        )
-                    )
+        async with (get_async_session_context() as session, 
+                    get_user_db_context(session) as user_db,
+                    get_user_manager_context(user_db) as user_manager):
+            await user_manager.create(
+                UserCreate(
+                    email=email,
+                    password=password,
+                    is_superuser=is_superuser
+                )
+            )
     except UserAlreadyExists:
         pass
 
